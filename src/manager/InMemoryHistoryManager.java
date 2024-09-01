8e9eb6d4dffe394ca6039472c23da0d9c79f197f
m.kiselev <sanwiraight@gmail.com>
2024-09-01 17:38:04 +0300

    [Sprint_6] Implement custom LinkedHashMap

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,34 +1,94 @@
 package manager;
 
 import model.Task;
+import util.Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history;
-    private final int maxSize = 10;
+    private final Map<Integer, Node<Task>> taskPositionsInNodeList;
+    private Node<Task> head;
+    private Node<Task> tail;
 
     public InMemoryHistoryManager() {
-        history = new ArrayList<>();
+        head = null;
+        tail = null;
+        taskPositionsInNodeList = new HashMap<>();
     }
 
     @Override
     public void add(Task task) {
-        history.add(task.getCopy());
-
-        if (history.size() > maxSize) {
-            history.remove(0);
-        }
+        task = task.getCopy();
+        remove(task.getId());
+        linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        return getTasks();
     }
 
     @Override
     public void remove(int id) {
-        return;
+        if (!taskPositionsInNodeList.containsKey(id)) {
+            return;
+        }
+
+        Node<Task> node = taskPositionsInNodeList.get(id);
+        removeNode(node);
+    }
+
+    private void linkLast(Task task) {
+        Node<Task> node = new Node<>(task);
+        taskPositionsInNodeList.put(task.getId(), node);
+
+        // if list is empty:
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+        }
+
+        tail = node;
+    }
+
+    private void removeNode(Node<Task> node) {
+        // if list is empty:
+        if (head == null) {
+            return;
+        }
+
+        Node<Task> prev = node.prev;
+        Node<Task> next = node.next;
+
+        // if prev is exists => node is not the head, else: node is the head
+        if (prev != null) {
+            prev.next = next;
+        } else {
+            head = next;
+        }
+
+        // if next is exists => node is not the tail, else: node is the tail
+        if (next != null) {
+            next.prev = prev;
+        } else {
+            tail = prev;
+        }
+    }
+
+    private List<Task> getTasks() {
+        List<Task> result = new ArrayList<>();
+        Node<Task> node = head;
+
+        while (node != null) {
+            result.add(node.data);
+            node = node.next;
+        }
+
+        return result;
     }
 }
