f6be94f3dddf3e303d12d0ecaaa8f667637771de
m.kiselev <sanwiraight@gmail.com>
2024-10-17 16:00:28 +0300

    [Sprint_9] Update Base handler

--- src/api/hadlers/BaseHttpHandler.java
+++ src/api/hadlers/BaseHttpHandler.java
@@ -6,14 +6,18 @@ import api.adapters.DurationAdapter;
 import api.adapters.LocalDateTimeAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import exceptions.ManagerTaskNotFound;
 import manager.TaskManager;
 
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public abstract class BaseHttpHandler implements HttpHandler {
     protected TaskManager manager;
@@ -30,16 +34,98 @@ public abstract class BaseHttpHandler implements HttpHandler {
                 .create();
     }
 
-    protected abstract void handleRequest(HttpExchange exchange) throws IOException;
-
     @Override
     public void handle(HttpExchange exchange) {
         try (exchange) {
-            handleRequest(exchange);
+            boolean handled = handleRequest(exchange);
+
+            if (!handled) {
+                sendNotFound(exchange);
+            }
         } catch (Exception exception) {
             System.out.println("Во время обработки запроса произошло непредвиденное исключение.");
             exception.printStackTrace();
         }
+    }
+
+    protected boolean handleRequest(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        String handlePath = getHandlePath();
+
+        String regExpWithId = "^" + handlePath + "/\\d+$";
+        String regExpWithoutId = "^" + handlePath + "$";
+
+        switch (method) {
+            case "GET": {
+                if (Pattern.matches(regExpWithId, path)) {
+                    handleGetTaskById(exchange, path);
+                    return true;
+                }
+
+                if (Pattern.matches(regExpWithoutId, path)) {
+                    showTasks(exchange);
+                    return true;
+                }
+
+                break;
+            }
+
+            case "POST": {
+                if (!Pattern.matches(regExpWithoutId, path)) {
+                    sendNotFound(exchange);
+                    return true;
+                }
+
+                String taskBody = new String(exchange.getRequestBody().readAllBytes(), Settings.DEFAULT_CHARSET);
+                JsonElement jsonElement = JsonParser.parseString(taskBody);
+
+                if (!jsonElement.isJsonObject()) {
+                    sendBadJsonBody(exchange);
+                    return true;
+                }
+
+                handlePost(exchange, taskBody);
+                return true;
+            }
+
+            case "DELETE": {
+                if (Pattern.matches(regExpWithId, path)) {
+                    handleDeleteTask(exchange, path);
+                    return true;
+                }
+
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    protected void handleGetTaskById(HttpExchange exchange, String path) throws IOException {
+        Optional<Integer> taskIdOpt = getIntFromSecondPathElement(path);
+
+        if (taskIdOpt.isEmpty()) {
+            sendInternalServerError(exchange, "Невозможно обработать ИД в запросе.");
+            return;
+        }
+
+        try {
+            showById(exchange, taskIdOpt.get());
+        } catch (ManagerTaskNotFound exception) {
+            sendNotFound(exchange, exception.getMessage());
+        }
+    }
+
+    protected void handleDeleteTask(HttpExchange exchange, String path) throws IOException {
+        Optional<Integer> taskIdOpt = getIntFromSecondPathElement(path);
+
+        if (taskIdOpt.isEmpty()) {
+            sendInternalServerError(exchange, "Невозможно обработать ИД в запросе.");
+            return;
+        }
+
+        deleteTask(taskIdOpt.get());
     }
 
     protected void sendOnlyStatus(HttpExchange h, int rCode) throws IOException {
@@ -68,7 +154,7 @@ public abstract class BaseHttpHandler implements HttpHandler {
     }
 
     protected void sendNotFound(HttpExchange h, String message) throws IOException {
-        sendStatusAndText(h, StatusCode.NOT_FOUND.getCode(), String.format("{message: %s}", message));
+        sendStatusAndText(h, StatusCode.NOT_FOUND.getCode(), String.format("{message: \"%s\"}", message));
     }
 
     protected void sendHasInteractions(HttpExchange h) throws IOException {
@@ -76,14 +162,28 @@ public abstract class BaseHttpHandler implements HttpHandler {
     }
 
     protected void sendInternalServerError(HttpExchange h, String error) throws IOException {
-        sendStatusAndText(h, StatusCode.INTERNAL_SERVER_ERROR.getCode(), String.format("{error: %s}", error));
+        sendStatusAndText(h, StatusCode.INTERNAL_SERVER_ERROR.getCode(), String.format("{error: \"%s\"}", error));
     }
 
-    protected Optional<Integer> getIntFromPath(String path, int position) {
+    protected void sendBadJsonBody(HttpExchange h) throws IOException {
+        sendStatusAndText(h, StatusCode.BAD_REQUEST.getCode(), "{error: \"Ошибка тела запроса. Ожидается JSON.\"}");
+    }
+
+    protected Optional<Integer> getIntFromSecondPathElement(String path) {
         try {
-            return Optional.of(Integer.parseInt(path.split("/")[position]));
+            return Optional.of(Integer.parseInt(path.split("/")[2]));
         } catch (NumberFormatException | IndexOutOfBoundsException | NullPointerException exception) {
             return Optional.empty();
         }
     }
+
+    protected abstract String getHandlePath();
+
+    protected abstract void showById(HttpExchange exchange, int id) throws ManagerTaskNotFound, IOException;
+
+    protected abstract void showTasks(HttpExchange exchange) throws IOException;
+
+    protected abstract void deleteTask(int id);
+
+    protected abstract void handlePost(HttpExchange exchange, String postBody) throws IOException;
 }
